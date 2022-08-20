3a07db6edfb576ea171ab9605873ee4652671efd
A <andcooke@gmail.com>
2022-08-20 11:15:51 -0500

    controllers psuedocoded

---
+++ controllers/thoughtController.js
@@ -0,0 +1,29 @@
+const { User, Thought } = require('../models');
+
+// `GET` to get all thoughts
+module.exports = {
+  //get all thoughts
+
+  //get a single thought
+
+  //create a thought (don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
+  // // example data
+  // {
+  //   "thoughtText": "Here's a cool thought...",
+  //   "username": "lernantino",
+  //   "userId": "5edff358a0fcb779aa7b118b"
+  // }
+  // ```
+
+  //update a thought 
+
+  //delete a thought
+
+
+  
+  //post to create a reaction stored in a single thought's reactions array field
+
+  //`DELETE` to pull and remove a reaction by the reaction's `reactionId` value
+}
+
+
